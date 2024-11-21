88fc36dbdcbad7ad0c0c5ff13616c5e937a6e1eb
jailton.ramos <jailton.ramos@omnilink.com.br>
2024-11-21 17:54:29 -0300

    Cria o DTO de Email

---
+++ src/main/java/com/ms/user/domain/EmailDTO.java
@@ -0,0 +1,18 @@
+package com.ms.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDTO {
+    private UUID userId;
+    private String emailTo;
+    private String subject;
+    private String text;
+}
+
